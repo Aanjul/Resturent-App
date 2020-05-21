6784b481ae8e893649dd609a0b8bdf11d3ff3f1a
Your Name <you@example.com>
2020-05-21 14:30:41 +0530

    axios update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from 'axios';
+import styled from 'styled-components'
+import Cuisine from './cuisine'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      meal: {}
+    }
+  }
+  componentDidMount(){
+    const URL = "https://www.themealdb.com/api/json/v1/1/latest.php";
+    
+    axios.get(URL)
+    .then(res => {
+      const meal = res.data.meals;
+      if(typeof meal === 'object'){
+        this.setState({meal});
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  
+  }
+  
+  render() {
+      var  data = this.state.meal;
+      return (
+        <AppContainer className="App">
+          {data.length > 0 && <Cuisine meals={data}/>}
+        </AppContainer>
+      
+    )
+  }
 }
+
+const AppContainer= styled.div
 
 export default App;
