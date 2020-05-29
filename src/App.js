79015dc583c15ae0b0e3053789f4eda7407c757a
Your Name <you@example.com>
2020-05-29 14:41:15 +0530

    react parsing updated some changes in App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import axios from 'axios';
-import styled from 'styled-components'
 import Cuisine from './cuisine'
 import './App.css';
 
@@ -13,33 +12,53 @@ class App extends Component {
       meal: {}
     }
   }
-  componentDidMount(){
-    const URL = "https://www.themealdb.com/api/json/v1/1/latest.php";
-    
-    axios.get(URL)
-    .then(res => {
-      const meal = res.data.meals;
-      if(typeof meal === 'object'){
-        this.setState({meal});
-      }
-    })
-    .catch(error => {
-      console.log(error);
-    });
-  
-  }
   
   render() {
       var  data = this.state.meal;
       return (
-        <AppContainer className="App">
-          {data.length > 0 && <Cuisine meals={data}/>}
-        </AppContainer>
-      
+      <div>
+      <div class="jumbotron jumbotron-billboard text-center">
+	<div class="img"></div>
+	<div class="container pt">
+		<div class="text-center">
+			<img class="logo" src='http://image.ibb.co/g52Ayb/Go_Food_2.png' />
+		</div>
+		<div class="mt-5 text-color">
+			<h1>Find the best Restaurants</h1>
+		</div>
+		<div class="row mx-auto well col-lg-12 mt-5">
+			<div class="col-xs-4 pr-0">
+				<select id="select_id" class="form-control form-control-lg">
+					<option value="Select City" hidden>Select City</option>
+					<option value="4">Bengaluru</option>
+					<option value="3" >Mumbai</option>
+					<option value="1">Delhi</option>
+					<option value="6">Hyderabad</option>
+					<option value="5">Pune</option>
+				</select>
+			</div>
+			<div class="col-xs-6 pl-0 pr-0">
+				<input id="getText" type="text" class="form-control" placeholder="Search for Restaurant"/>
+				</div>
+				<button id = "getMessage" class = "btn btn-success col-xs-2">Search</button>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div>
+			<div class = "message col-md-12"></div>
+		</div>
+	</div>
+      <div className="App">
+        <div> Restaurant ID </div>
+        <div> Restaurant Name </div>
+        <div> Cuisines </div>
+        <div> Average Cost for Two </div>
+        <div> Table Bookings</div>
+      </div>
+      </div>
     )
   }
 }
-
-const AppContainer= styled.div
 
 export default App;
